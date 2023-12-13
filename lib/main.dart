9580cf8befb1390d9c0a7e2414f4eb996a41ec21
Shah Raza <shahsomething@yahoo.com>
2023-12-13 21:09:17 +0500

    App theme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movies/app/app.bottomsheets.dart';
 import 'package:movies/app/app.dialogs.dart';
 import 'package:movies/app/app.locator.dart';
 import 'package:movies/app/app.router.dart';
 import 'package:movies/app/app.snackbars.dart';
+import 'package:movies/ui/common/app_theme.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 Future<void> main() async {
@@ -18,15 +20,26 @@ Future<void> main() async {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  static const double _designWidth = 375;
+  static const double _designHeight = 812;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: Routes.startupView,
-      onGenerateRoute: StackedRouter().onGenerateRoute,
-      navigatorKey: StackedService.navigatorKey,
-      navigatorObservers: [
-        StackedService.routeObserver,
-      ],
+    return ScreenUtilInit(
+      designSize: const Size(_designWidth, _designHeight),
+      builder: (context, _) {
+        return MaterialApp(
+          initialRoute: Routes.startupView,
+          theme: AppTheme.lightTheme,
+          themeMode: ThemeMode.light,
+          onGenerateRoute: StackedRouter().onGenerateRoute,
+          navigatorKey: StackedService.navigatorKey,
+          navigatorObservers: [
+            StackedService.routeObserver,
+          ],
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
--- lib/ui/common/app_colors.dart
+++ lib/ui/common/app_colors.dart
@@ -4,7 +4,7 @@ class AppColors {
   static const Color primaryColor = Color.fromRGBO(46, 39, 57, 1);
   static const Color blackColor = Color(0xFF000000);
   static const Color greyColor = Color(0xFF827D88);
-  static const Color lightGreyColorD = Color(0xFFDBDBDF);
+  static const Color lightGreyColor = Color(0xFFDBDBDF);
   static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color backgoundColor = Color(0xFFF6F6FA);
   static const Color skyBlueColor = Color(0xFF61C3F2);
