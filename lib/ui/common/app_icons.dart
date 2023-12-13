06736d87e7f94fe331086d23c0c1f219b98c416c
Shah Raza <shahsomething@yahoo.com>
2023-12-13 19:48:25 +0500

    App Icons added

---
+++ lib/ui/common/app_icons.dart
@@ -0,0 +1,12 @@
+class AppIcons {
+  static const String path = 'assets/icons';
+
+  //Navigation Icons
+  static const String dashboardIcon = '$path/dashboard.svg';
+  static const String watchIcon = '$path/watch.svg';
+  static const String libraryIcon = '$path/library.svg';
+  static const String moreIcon = '$path/more.svg';
+
+  //Others
+  static const String searchIcon = '$path/search.svg';
+}
